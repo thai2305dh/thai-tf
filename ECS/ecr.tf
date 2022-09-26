97a0712c3c71c6dda25b5a5d3176ef8147c10023
thai2305dh <56582536+thai2305dh@users.noreply.github.com>
2022-09-26 09:51:33 +0700

    Update ecr.tf

--- ECS/ecr.tf
+++ ECS/ecr.tf
@@ -1,11 +1,11 @@
 
 resource "aws_ecr_repository" "ecr" {
   name = "terraform"
-
-  # Khả năng thay đổi Tags
+  
+# Khả năng thay đổi Tags
   image_tag_mutability = "IMMUTABLE"
 
-  # Scan iamge on Repo
+# Scan iamge on Repo
   image_scanning_configuration {
     scan_on_push = true
   }
