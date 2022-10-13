5c2966be4bd5fc364575a66ed21e2dc460849955
thai2305dh <thaidhn23@gmail.com>
2022-10-13 11:12:56 +0700

    dada

---
+++ variables.tf
@@ -0,0 +1,10 @@
+# VPC
+variable "vpc_cidr_block" {}
+variable "public_subnet" {}
+variable "map_az_subnet" {}
+
+# ELB
+variable "name" {}
+variable "instance_type" {}
+variable "key_pair" {}
+variable "image_id" {}
